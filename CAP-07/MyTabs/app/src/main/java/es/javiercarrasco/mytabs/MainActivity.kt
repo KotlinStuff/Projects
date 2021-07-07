a87e62a955bf64f5ab92e86f57ef995903c93612
Javier Carrasco <carrasco.javi@gmail.com>
2021-07-07 13:58:14 +0200

    Capítulo 7

---
+++ CAP-07/MyTabs/app/src/main/java/es/javiercarrasco/mytabs/MainActivity.kt
@@ -0,0 +1,34 @@
+package es.javiercarrasco.mytabs
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import es.javiercarrasco.mytabs.adapters.ViewPagerAdapter
+import es.javiercarrasco.mytabs.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Se carga la toolbar.
+        setSupportActionBar(binding.toolbar)
+
+        // Se crea el adapter.
+        val adapter = ViewPagerAdapter(supportFragmentManager)
+
+        // Se añaden los fragments y los títulos de pestañas.
+        adapter.addFragment(PersonsFragment(), "Personas")
+        adapter.addFragment(FruitsFragment(), "Frutas")
+        adapter.addFragment(AnimalsFragment(), "Animales")
+
+        // Se asocia el adapter.
+        binding.viewPager.adapter = adapter
+
+        // Se cargan las tabs.
+        binding.tabs.setupWithViewPager(binding.viewPager)
+    }
+}
